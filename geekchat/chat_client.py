f49d142af17279c1e8549cbc18dde5080462b91a
Cerzon <kirill.tkachenko@gmail.com>
2021-03-19 18:25:30 +0300

    threads for user input and socket receive

--- geekchat/chat_client.py
+++ geekchat/chat_client.py
@@ -1,4 +1,7 @@
 from contextlib import closing
+from socket import socket
+from threading import Thread
+from queue import Queue
 
 from .protocol import compose_request, ACTION_TPLS, ACT_MSG, ACT_PRESENCE
 from .transport import chat_socket, send_data, receive_data
@@ -10,30 +13,26 @@ start_logger()
 
 
 @logged
-def start_client(addr: str, port: int, sender: bool=False):
-    """ If sender is True - only sends data to server,
-        otherwise listen only
-    """
+def start_client(addr: str, port: int):
+
     client = chat_socket(addr, port)
+    user_input_queue = Queue()
+    received_data_queue = Queue()
 
     with closing(client):
 
         account_name = ''
 
-        if sender:
-            while not account_name:
-                account_name = input('login: ')
-            status = input('status: ')
+        while not account_name:
+            account_name = input('login: ')
 
-            # initial request-response session
-            rqst = compose_request(ACT_PRESENCE, account_name, status)
-            send_data(client, rqst)
+        Thread(target=user_input_loop, args=(user_input_queue,)).start()
+        Thread(target=receive_data_loop, args=(client, received_data_queue)).start()
 
         # main loop
         while True:
-            if sender:
-                print('type /quit for quit')
-                user_input = input('Message: ')
+            if not user_input_queue.empty():
+                user_input = user_input_queue.get()
                 rqst = None
 
                 # parse user input
@@ -45,9 +44,27 @@ def start_client(addr: str, port: int, sender: bool=False):
                 elif user_input:
                     rqst = compose_request(ACT_MSG, 'somebody',
                                             account_name, '', user_input)
+                
+                user_input_queue.task_done()
 
                 if rqst:
                     send_data(client, rqst)
-            else:
-                resp = receive_data(client)
+
+            if not received_data_queue.empty():
+                resp = received_data_queue.get()
                 print(resp)
+                received_data_queue.task_done()
+
+
+@logged
+def user_input_loop(user_input_queue: Queue):
+    while True:
+        user_input = input()
+        if user_input:
+            user_input_queue.put_nowait(user_input)
+
+
+@logged
+def receive_data_loop(src_sock: socket, received_data_queue: Queue):
+    while True:
+        received_data_queue.put_nowait(receive_data(src_sock))
